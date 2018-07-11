f1757a10e0bd07808a4d8a331da537f07b94079d
Carlos Bellino <carlosbellino@gmail.com>
2018-07-11 16:49:29 +0200

    User as entity

--- src/main/java/com/kaname/restapisecurity/domain/User.java
+++ src/main/java/com/kaname/restapisecurity/domain/User.java
@@ -4,33 +4,32 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import static java.util.Objects.requireNonNull;
+import static javax.persistence.GenerationType.IDENTITY;
 
-@Value
+@Entity
+@Data
 @Builder
 public class User implements UserDetails {
-    private static final long serialVersionUID = 2396654715019746670L;
 
-    String id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    Long id;
+
     String username;
     String password;
-
-    @JsonCreator
-    User(@JsonProperty("id") final String id,
-         @JsonProperty("username") final String username,
-         @JsonProperty("password") final String password) {
-        super();
-        this.id = requireNonNull(id);
-        this.username = requireNonNull(username);
-        this.password = requireNonNull(password);
-    }
+    String token;
 
     @JsonIgnore
     @Override
